85b7efb504e268c289957aa4c3ab73d2611ff7e9
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-12-20 23:42:55 +0000

    added mipmap from tutorial

--- openglgame/src/main/java/com/gopiandcode/render/Loader.java
+++ openglgame/src/main/java/com/gopiandcode/render/Loader.java
@@ -33,6 +33,9 @@ public class Loader {
         Texture texture = null;
         try {
             texture = TextureLoader.getTexture("PNG", new FileInputStream("res/" + filename + ".png"));
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.5f);
         } catch (IOException e) {
             e.printStackTrace();
         }
