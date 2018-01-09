7a522dfcade71775623eea32301c2d2d5892eb9c
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-09 22:46:16 +0000

    improved visualiztions

---
+++ gopiandcode-xcs/src/main/java/com/gopiandcode/lcs/rcs/RCSClassifier.java
@@ -0,0 +1,7 @@
+package com.gopiandcode.lcs.rcs;
+
+/**
+ * Created by Gopiandcode on 09/01/2018.
+ */
+public class RCSClassifier {
+}
---
+++ gopiandcode-xcs/src/main/java/com/gopiandcode/lcs/util/Either.java
@@ -0,0 +1,7 @@
+package com.gopiandcode.lcs.util;
+
+/**
+ * Created by Gopiandcode on 09/01/2018.
+ */
+public class Either {
+}
